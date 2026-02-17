39a74214e817d30b39c64a6f0f997dc74a840915
Flávio <f.augustosdn@gmail.com>
2026-02-17 19:58:53 -0300

    fix: adicionada annotation para que não deixe o campo nullable

--- src/main/java/br/com/flavioaugusto/gestao_vagas/entity/CandidateEntity.java
+++ src/main/java/br/com/flavioaugusto/gestao_vagas/entity/CandidateEntity.java
@@ -1,14 +1,9 @@
 package br.com.flavioaugusto.gestao_vagas.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.Pattern;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.validator.constraints.Length;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
@@ -23,9 +18,18 @@ public class CandidateEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
+
+    @Column(nullable = false)
     private String name;
+
+    @Column(nullable = false)
     private String username;
+
+    @NotNull
+    @Column(unique = true)
     private String email;
+
+    @Column(nullable = false)
     private String password;
     private String description;
     private String curriculum;
